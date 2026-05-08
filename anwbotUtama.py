a8737825b253f29ebd195eb4d20f22c9f295aec0
ANW®©™✓ <carpkingrc1@gmail.com>
2026-05-08 18:46:36 +0800

    Fix Flask app initialization

--- anwbotUtama.py
+++ anwbotUtama.py
@@ -4,7 +4,7 @@ from flask import Flask
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # --- ENGINE RENDER ---
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home(): 

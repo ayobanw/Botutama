0bd7f55788c45eae0492bc877662754079ed1284
ANW®©™✓ <carpkingrc1@gmail.com>
2026-05-08 17:22:11 +0800

    Update anwbotUtama.py

--- anwbotUtama.py
+++ anwbotUtama.py
@@ -33,15 +33,13 @@ def main_menu():
 def is_subscribed(user_id):
     try:
         member = bot.get_chat_member(CHANNEL_ID, user_id)
-        if member.status in ['creator', 'administrator', 'member']:
-            return True
-        return False
+        return member.status in ['creator', 'administrator', 'member']
     except:
         return False
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    # KHAS: Owner (kau) terus lepas masuk menu. User lain wajib join.
+    # ID kau (1159846205) terus lepas menu. Orang lain wajib join group.
     if msg.from_user.id == 1159846205 or is_subscribed(msg.from_user.id):
         bot.send_photo(msg.chat.id, IMAGE_URL, caption="🏠 *Menu Utama ANW STORE*", reply_markup=main_menu())
     else:
@@ -58,46 +56,109 @@ def check_status(call):
     else:
         bot.answer_callback_query(call.id, "⚠️ Awak belum join lagi!", show_alert=True)
 
-# --- SEMUA LIST DATA ASAL KAU (Dah Masukkan Balik Semua) ---
+# --- LIST DATA ASAL KAU (SUSUN KE BAWAH) ---
 @bot.callback_query_handler(func=lambda c: c.data == "kereta_menu")
 def kereta_menu(call):
     text = (
         "🏎️ *LIST CODE KERETA*\n\n"
-        "202 Pagani Zonda\n204 Mercedes Van\n205 Porsche RWB\n206 Alfa Romeo Giulia\n"
-        "207 Porsche Le Mans\n208 Ford Bronco\n209 Lexus IS300 2009\n210 Corvette C5\n"
-        "211 Audi RS7\n212 6x6 Mercedes G63\n213 Nissan R33\n214 Toyota Chaser MK2\n"
-        "215 Chevy Tahoe\n216 McLaren Senna\n217 BMW X7\n218 Toyota Crown\n"
-        "219 VW Passat\n220 Fairlady Z\n221 Old NSX\n222 Porsche 918\n"
-        "223 DMC Delorean\n224 Subaru Raptor Eye\n225 Honda DelSol\n226 Fiat Van\n"
-        "227 AMG One\n228 Audi RS2\n229 Ferrari F40\n230 Land Rover\n"
-        "231 Toyota LC250 Old\n232 Kia Stinger\n233 BMW i7\n234 Newer Aston Martin\n"
-        "235 Newer Mustang\n236 RV\n237 Semi Truck\n238 New BMW M5\n"
-        "239 New Escalade\n240 Ford Focus\n241 LaFerrari\n242 New Camaro\n"
-        "243 Jeep Gladiator\n244 New Land Rover\n245 Toyota GR Yaris\n248 New Russian Car\n"
-        "249 New Maybach\n250 Porsche Carrera GT\n253 FORD TURCKS NEW\n257 BMW M2 NEW\n259 FORTUNER NEW"
+        "202 Pagani Zonda\n"
+        "204 Mercedes Van\n"
+        "205 Porsche RWB\n"
+        "206 Alfa Romeo Giulia\n"
+        "207 Porsche Le Mans\n"
+        "208 Ford Bronco\n"
+        "209 Lexus IS300 2009\n"
+        "210 Corvette C5\n"
+        "211 Audi RS7\n"
+        "212 6x6 Mercedes G63\n"
+        "213 Nissan R33\n"
+        "214 Toyota Chaser MK2\n"
+        "215 Chevy Tahoe\n"
+        "216 McLaren Senna\n"
+        "217 BMW X7\n"
+        "218 Toyota Crown\n"
+        "219 VW Passat\n"
+        "220 Fairlady Z\n"
+        "221 Old NSX\n"
+        "222 Porsche 918\n"
+        "223 DMC Delorean\n"
+        "224 Subaru Raptor Eye\n"
+        "225 Honda DelSol\n"
+        "226 Fiat Van\n"
+        "227 AMG One\n"
+        "228 Audi RS2\n"
+        "229 Ferrari F40\n"
+        "230 Land Rover\n"
+        "231 Toyota LC250 Old\n"
+        "232 Kia Stinger\n"
+        "233 BMW i7\n"
+        "234 Newer Aston Martin\n"
+        "235 Newer Mustang\n"
+        "236 RV\n"
+        "237 Semi Truck\n"
+        "238 New BMW M5\n"
+        "239 New Escalade\n"
+        "240 Ford Focus\n"
+        "241 LaFerrari\n"
+        "242 New Camaro\n"
+        "243 Jeep Gladiator\n"
+        "244 New Land Rover\n"
+        "245 Toyota GR Yaris\n"
+        "248 New Russian Car\n"
+        "249 New Maybach\n"
+        "250 Porsche Carrera GT\n"
+        "253 FORD TURCKS NEW\n"
+        "257 BMW M2 NEW\n"
+        "259 FORTUNER NEW"
     )
     kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton("⬅ Kembali", callback_data="back_menu"))
     bot.edit_message_text(text, call.message.chat.id, call.message.message_id, reply_markup=kb)
 
 @bot.callback_query_handler(func=lambda c: c.data == "custom_menu")
-
 def custom_menu(call):
     text = (
         "🛠️ *CODE CUSTOM SPOILER DAN BESI DALAM*\n\n"
-        "4 - Besi ampaian/Bmw x6\n5 - Bar depan\n6 - roofbox/skybox\n10 - Lampu roof\n"
-        "12 - besi rack lata\n13 - host lata\n14 - roof RR\n15 - besi blakang ford rangers\n"
-        "20 - besi rack hakosuka\n32 - tire blakang jeep\n33 - besi atas jeep\n"
-        "34 - besi blakang jeep\n39 - besi dalam dodge\n45 - besi atas hammer\n"
-        "46/47 - besi blakang hilux\n48 - besi atas hilux\n50/52 - besi bar atas hilux\n"
-        "56 - sportlight jeep\n59/60 - rack ford rangers\n61/62 - besi blakang hilux\n"
-        "63 - besi blakang hilux papan\n69 - besi rack atas myvi\n75 - besi chavrolet\n\n"
+        "4 - Besi ampaian/Bmw x6\n"
+        "5 - Bar depan\n"
+        "6 - roofbox/skybox\n"
+        "10 - Lampu roof\n"
+        "12 - besi rack lata\n"
+        "13 - host lata\n"
+        "14 - roof RR\n"
+        "15 - besi blakang ford rangers\n"
+        "20 - besi rack hakosuka\n"
+        "32 - tire blakang jeep\n"
+        "33 - besi atas jeep\n"
+        "34 - besi blakang jeep\n"
+        "39 - besi dalam dodge\n"
+        "45 - besi atas hammer\n"
+        "46/47 - besi blakang hilux\n"
+        "48 - besi atas hilux\n"
+        "50/52 - besi bar atas hilux\n"
+        "56 - sportlight jeep\n"
+        "59/60 - rack ford rangers\n"
+        "61/62 - besi blakang hilux\n"
+        "63 - besi blakang hilux papan\n"
+        "69 - besi rack atas myvi\n"
+        "75 - besi chavrolet\n\n"
         "📍 *CODE SPOILER PILIHAN*\n"
-        "16 - spoiler merc Amg\n19 - spoiler r35\n41 - spoiler 350z\n49 - spoiler brz\n"
-        "65 - spoiler rx7\n69 - spoiler nsx\n77 - spoiler rx8\n93 - spoiler Itik\n"
-        "95 - spoiler ducktail infinity\n96 - spoiler wing infinity\n106 - spoiler wing ek9\n"
-        "135 - spoiler audi r8v8\n148 - spoiler ferrari\n155 - spoiler wing bmw\n"
-        "168 - spoiler Evo\n170 - spoiler Lambo"
+        "16 - spoiler merc Amg\n"
+        "19 - spoiler r35\n"
+        "41 - spoiler 350z\n"
+        "49 - spoiler brz\n"
+        "65 - spoiler rx7\n"
+        "69 - spoiler nsx\n"
+        "77 - spoiler rx8\n"
+        "93 - spoiler Itik\n"
+        "95 - spoiler ducktail infinity\n"
+        "96 - spoiler wing infinity\n"
+        "106 - spoiler wing ek9\n"
+        "135 - spoiler audi r8v8\n"
+        "148 - spoiler ferrari\n"
+        "155 - spoiler wing bmw\n"
+        "168 - spoiler Evo\n"
+        "170 - spoiler Lambo"
     )
     kb = InlineKeyboardMarkup()
     kb.add(InlineKeyboardButton("⬅ Kembali", callback_data="back_menu"))
@@ -107,6 +168,7 @@ def custom_menu(call):
 def back_menu(call):
     bot.edit_message_text("🏠 *Menu Utama*", call.message.chat.id, call.message.message_id, reply_markup=main_menu())
 
+# --- FIX NAME ERROR (TANDA __ DAH BETUL) ---
 if name == "main":
     t = threading.Thread(target=run)
     t.daemon = True
